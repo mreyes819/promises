345649de8c677d0cf92d360d8e522290ef9e2924
mttrys <mttrys@gmail.com>
2017-06-26 22:47:32 -0700

    Implement promiseConstructor

--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -11,8 +11,8 @@ var Promise = require('bluebird');
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
   return new Promise((resolve, reject) => {
-    fs.readFile(filePath, 'utf8', function(error, data){
-      if(error) {
+    fs.readFile(filePath, 'utf8', function(error, data) {
+      if (error) {
         reject(error);
       } else {
         let firstLine = data.split('\n')[0];
@@ -24,8 +24,17 @@ var pluckFirstLineFromFileAsync = function(filePath) {
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
-  // TODO
-};
+  return new Promise((resolve, reject) => {
+    request(url, function (error, response, body) {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(response && response.statusCode);
+      }
+    });
+  });
+}
+
 
 // Export these functions so we can test them and reuse them in later exercises
 module.exports = {
